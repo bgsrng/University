cf21cde5961bc777e1adce33eaf5aa94fa07e581
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-03-29 14:14:31 +0700

    database added

---
+++ src/configs/db.js
@@ -0,0 +1,32 @@
+const mahasiswaDBConfig = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const dosenDBConfig = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const matkuliahDBConfig = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const clubDBConfig = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const fakultasDBConfig = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+
+module.exports = { mahasiswaDBConfig, matkuliahDBConfig, fakultasDBConfig, clubDBConfig, dosenDBConfig };
---
+++ src/database/index.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_DBNAME, MYSQL_PORT } = require('../configs/db');
+const util = require('util');
+
+const mahasiswaDB = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const dosenDB = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const matkuliah = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const clubDB = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
+const fakultas = {
+  MYSQL_USER: '',
+  MYSQL_PASSWORD: '',
+  MYSQL_DBNAME: '',
+  MYSQL_PORT: 0,
+};
